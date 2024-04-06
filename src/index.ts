fe5bfd19e7a8452ee31497df1763b79e0282f020
Eko Saputra <Ekosaputra.t.i@gmail.com>
2024-04-06 11:16:49 +0700

    corsa

--- src/index.ts
+++ src/index.ts
@@ -57,6 +57,7 @@ const app = express()
 
 const allowedOrigins = [
     'http://localhost:5173',
+    'http://localhost:5173/',
     'https://app.espbimbel.com',
     'https://app.espbimbel.com/',
     'http://app.espbimbel.com',
