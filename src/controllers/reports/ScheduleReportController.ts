5b0c77f0bbfdd48681587e9040fb7ae444dd3a0d
Eko Saputra <Ekosaputra.t.i@gmail.com>
2024-08-05 15:07:08 +0700

    schedule

--- src/controllers/reports/ScheduleReportController.ts
+++ src/controllers/reports/ScheduleReportController.ts
@@ -13,46 +13,79 @@ const getData = async (req:Request, res:Response) => {
         body.tentor ? filter = {...filter, tentorId: body.tentor?.value} : null;
         body.studyGroup ? filter = {...filter, studyGroupId: body.studyGroup?.value} : null;
         body.student ? filter = {...filter, scheduleDetails: {
-            every: {
+            some: {
                 studentId: body.student?.value
             }
         }} : null
+        
         body.scheduleType ? filter = {...filter, scheduleType: body.scheduleType?.value} : null;
-        const data = await Model.schedules.findMany({
+        // const data = await Model.schedules.findMany({
+        //     where: {
+        //         AND: {
+        //             // date: {
+        //             // gte: moment(body.startDate+' 00:00:00').format(),
+        //             // lte: moment(body.endDate+' 23:59:00').format(),
+        //             // },
+        //             scheduleDetails: {
+        //                 some: {
+        //                     studentId: body.student?.value
+        //                 }
+        //             }
+        //         }
+                
+        //         // ...filter
+        //     },
+        //     include: {
+        //         tentor: true,
+        //         rooms: true,
+        //         courses: true,
+        //         studyGroups: true,
+        //         classTypes: true,
+        //     },
+        //     orderBy: {
+        //         date: 'asc'
+        //     }
+        // });
+
+        const data = await Model.scheduleDetails.findMany({
             where: {
-                date: {
-                    gte: moment(body.startDate+' 00:00:00').format(),
-                    lte: moment(body.endDate+' 23:59:00').format(),
-                },
-                ...filter
+                studentId: body.student?.value,
+                schedules: {
+                    date: {
+                        gte: moment(body.startDate+' 00:00:00').format(),
+                        lte: moment(body.endDate+' 23:59:00').format(),
+                    }
+                }
             },
             include: {
-                tentor: true,
-                rooms: true,
+                schedules: {
+                    include: {
+                        tentor: true,
+                        rooms: true,
                 courses: true,
                 studyGroups: true,
                 classTypes: true,
-            },
-            orderBy: {
-                date: 'asc'
+                    }
+                }
             }
-        });
+        })
 
         let newData:any=[]
         let number:number=1;
         for (const value of data) {
+            
             newData=[
                 ...newData,
                 [
                     number,
-                    value.tentor?.name,
-                    value.studyGroups?.name ?? '',
-                    moment(value.date).tz('Asia/Jakarta').format('DD-MM-YYYY HH:mm:ss'),
-                    value.courses?.name,
-                    value.rooms?.name,
-                    value.type==="study" ? "Belajar" : "Try Out",
-                    value.method,
-                    value.classTypes?.name
+                    value?.schedules?.tentor?.name,
+                    value?.schedules?.studyGroups?.name ?? '',
+                    moment(value.schedules?.date).tz('Asia/Jakarta').format('DD-MM-YYYY HH:mm:ss'),
+                    value.schedules?.courses?.name,
+                    value.schedules?.rooms?.name,
+                    value.schedules?.type==="study" ? "Belajar" : "Try Out",
+                    value.schedules?.method,
+                    value.schedules?.classTypes?.name
                 ]
             ];
             number++
